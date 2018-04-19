d2b0832dcfb48e5bbe557153415324f7029b8373
ozielalves <ozielalves7@gmail.com>
2018-04-19 20:10:47 -0300

    Game editing

--- src/game.cpp
+++ src/game.cpp
@@ -5,5 +5,47 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 
+/// Define X and Y as the width and the hight
+static void nextGeneration(){
+
+/// Finding neighbours alive
+	count = 0;
+	for(int i=xpos-1; i < xpos+2; i++) {
+		if(rhs->ptr_M[ypos-1][i] == rhs.live_cell) {
+			count++;
+		}
+					
+	}
+	for(int i=xpos-1; i < xpos+2; i++) {
+		if(rhs->ptr_M[ypos][i] == rhs.live_cell) {
+			if( i == xpos ) continue;
+				count++;
+																	
+		}
+					
+	}
+	for(int i=xpos-1; i < xpos+2; i++) {
+		if(rhs->ptr_M[ypos+1][i] == rhs.live_cell) {
+			count++;
+			}
+
+	}
+}
+
+Borders correct_borders(  int m_y, int m_x, Borders *fix_border  ){
+		
+		if( height == 0  )
+					fix_border->ymin = 0;
+			if( width == 0  )
+						fix_border->xmin = 0;
+				if( height == screen->height-1  )
+							fix_border->ymax = 0;
+					if( width == screen->width-1  )
+								fix_border->xmax = 0;
+
+						return fix_border;
+
+}

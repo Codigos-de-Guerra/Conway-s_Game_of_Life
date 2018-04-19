939150f20c72fad263c45792d6ca1ce49991b463
Daniel Oliveira Guerra <daniel.guerra13@hotmail.com>
2018-04-19 20:09:06 -0300

    Uhu

--- src/main.cpp
+++ src/main.cpp
@@ -185,11 +185,12 @@ class Cell {
 };
 int main(int argc, char **argv) {
 
-/*-------------------Checking command line section----------------------------------*/
+/*-------------------Checking command line section----------------------------*/
 	if(argc != 2) {
 		std::cerr << "Invalid command line input!\n";
 		return -1;
 	}
+
 	std::istringstream ss(argv[1]);
 	std::string in_filename;
 	if(!(ss >> in_filename)) {
@@ -214,7 +215,7 @@ int main(int argc, char **argv) {
 	sel.future(cel);
 	sel.print();
 
-
+	ifs.close();
 
 	return 0;
 }
